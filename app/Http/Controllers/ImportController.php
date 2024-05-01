fdb68723f3822cf52d618b8ce491e9d7edbf7f24
Zulfadli Rizal <dev@bprbangunarta.co.id>
2024-05-01 18:11:35 +0700

    SIMONTOK

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -8,6 +8,7 @@ use App\Imports\TunggakanImport;
 use App\Imports\UsersImport;
 use App\Imports\WriteoffImport;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ImportController extends Controller
@@ -25,6 +26,35 @@ class ImportController extends Controller
         Excel::import(new UsersImport, public_path('/import/users/' . $fileName));
 
         return redirect()->route('user.index')->with('success', 'User berhasil diimport');
+    }
+
+    public function import(Request $request)
+    {
+        $request->validate([
+            'file' => 'required|mimes:csv,xls,xlsx'
+        ]);
+
+        $file = $request->file('file');
+
+        // membuat nama file unik
+        $nama_file = $file->hashName();
+
+        //temporary file
+        $path = $file->storeAs('public/excel/', $nama_file);
+
+        // import data
+        $import = Excel::import(new UsersImport(), storage_path('app/public/excel/' . $nama_file));
+
+        //remove from server
+        Storage::delete($path);
+
+        if ($import) {
+            //redirect
+            return redirect()->route('users.index')->with(['success' => 'Data Berhasil Diimport!']);
+        } else {
+            //redirect
+            return redirect()->route('users.index')->with(['error' => 'Data Gagal Diimport!']);
+        }
     }
 
     public function kredit(Request $request)
