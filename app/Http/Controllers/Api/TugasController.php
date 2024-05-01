3669fa53b641b64b591da424f0c4aac38ae57e11
Zulfadli Rizal <dev@bprbangunarta.co.id>
2024-05-01 18:37:49 +0700

    SIMONTOK

--- app/Http/Controllers/Api/TugasController.php
+++ app/Http/Controllers/Api/TugasController.php
@@ -23,30 +23,6 @@ class TugasController extends Controller
         return new TugasResource($tugas);
     }
 
-    // public function upload(Request $request)
-    // {
-    //     $request->validate([
-    //         'notugas'           => 'required',
-    //         'foto_pelaksanaan'  => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:5120',
-    //     ], [
-    //         'notugas.required'          => 'No Tugas tidak boleh kosong',
-    //         'foto_pelaksanaan.required' => 'Foto tidak boleh kosong',
-    //         'foto_pelaksanaan.image'    => 'Foto harus berupa gambar',
-    //         'foto_pelaksanaan.mimes'    => 'Foto harus berformat jpeg, png, jpg, gif, atau svg',
-    //         'foto_pelaksanaan.max'      => 'Ukuran foto maksimal 5MB',
-    //     ]);
-
-    //     $notugas    = $request->notugas;
-    //     $fileName   = $notugas . '.' . $request->foto_pelaksanaan->getClientOriginalExtension();
-    //     $request->foto_pelaksanaan->move(public_path('images/tugas'), $fileName);
-
-    //     $tugas = Tugas::where('notugas', $notugas)->first();
-    //     $tugas->foto_pelaksanaan = $fileName;
-    //     $tugas->save();
-
-    //     return new TugasResource($tugas);
-    // }
-
     public function upload(Request $request)
     {
         $request->validate([
