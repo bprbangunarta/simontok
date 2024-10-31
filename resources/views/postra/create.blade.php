1b3ac681b10de6ad434b909afe63cf7637dbd801
Zulfadli Rizal <it@bprbangunarta.co.id>
2024-10-31 23:01:08 +0700

    POSTRA

--- resources/views/postra/create.blade.php
+++ resources/views/postra/create.blade.php
@@ -29,7 +29,7 @@
                             </div>
 
                             <div class="mb-0">
-                                <button type="submit" class="btn btn-primary waves-effect waves-light w-100">Upload</button>
+                                <button type="submit" class="btn btn-primary waves-effect waves-light w-100" disabled>Upload</button>
                             </div>
                         </div>
                     </div>
@@ -163,6 +163,45 @@
                                                     </a>
                                                 </td>
                                             </tr>
+
+                                            <div class="modal fade" id="modal{{ $item->noreg }}" tabindex="-1" aria-hidden="true">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header mb-3">
+                                                            <h5 class="modal-title" id="exampleModalLabel1">Verifikasi Agunan</h5>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+
+                                                        <form action="{{ route('postra.update.agunan', $item->noreg) }}" method="POST">
+                                                            @csrf
+                                                            @method('PUT')
+                                                            <div class="modal-body" style="margin-top: -20px;">
+                                                                <div class="row">
+                                                                    <div class="d-flex justify-content-between">
+                                                                        <label class="form-label">Kondisi Agunan</label>
+                                                                    </div>
+                                                                    <div class="col">
+                                                                        <textarea class="form-control" name="kondisi" rows="3" required>{{ $item->kondisi }}</textarea>
+                                                                    </div>
+                                                                </div>
+
+                                                                <div class="row mt-3">
+                                                                    <div class="d-flex justify-content-between">
+                                                                        <label class="form-label">Penguasaan Agunan</label>
+                                                                    </div>
+                                                                    <div class="col">
+                                                                        <textarea class="form-control" name="penguasaan" rows="3" required>{{ $item->penguasaan }}</textarea>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-label-secondary" data-bs-dismiss="modal">Tutup</button>
+                                                                <button type="submit" class="btn btn-primary" disabled>Update</button>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
                                             @empty
                                             <tr>
                                                 <td colspan=" 2" class="text-center">Data tidak ditemukan
